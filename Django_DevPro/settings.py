002f253dcd7e684f487a9ee60082a01bae609ce2
Leonardo Couto <unknown>
2021-12-05 02:17:33 -0300

    [#32] Desacopla configurações de debug do Django
    Utiliza biblioteca "python-decouple" para usar diferentes configurações de debug para o Django a depender do local onde seu servidor está rodando. Torna função de debug do Django habilitada apenas quando servidor for rodado localmente.
    
    Disponibiliza sample de arquivo de configuração de variável na pasta "contrib".
    
    Configura o servidor de integração contínua para utilizar configuração presente no sample criado.
    
    [Close #32]

--- Django_DevPro/settings.py
+++ Django_DevPro/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(rc^uksfg08!i-oah_5-lbzzdw(8-d$tos#prw^u5@k67e3kx9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
