d5feef0715aae0e1e8d9fe44b77781233add7a2c
Leonardo Couto <>
2022-07-13 00:04:50 -0300

    Altera configuração do Collectfast para teste

--- Django_DevPro/settings.py
+++ Django_DevPro/settings.py
@@ -33,6 +33,9 @@ AUTH_USER_MODEL = 'base.User'
 
 # Application definition
 
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+COLLECTFAST_STRATEGY = 'collectfast.strategies.boto3.Boto3Strategy'
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -143,8 +146,6 @@ if AWS_ACCESS_KEY_ID:
     AWS_S3_CUSTOM_DOMAIN = None
 
     COLLECTFAST_ENABLED = True
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    COLLECTFAST_STRATEGY = 'collectfast.strategies.boto3.Boto3Strategy'
 
     AWS_DEFAULT_ACL = 'private'
 
