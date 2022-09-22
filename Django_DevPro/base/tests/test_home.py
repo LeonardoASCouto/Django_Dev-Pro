977d068effcf159050d1bee7d0b1d49e94684a38
Leonardo Couto <unknown>
2022-09-22 11:43:54 -0300

    [#268] Expõe módulo Django assertions e altera title da landing page
    [Close #268]

--- Django_DevPro/base/tests/test_home.py
+++ Django_DevPro/base/tests/test_home.py
@@ -1,6 +1,12 @@
 from django.test import Client
+from Django_DevPro.django_assertions import assert_contains
 
 
 def test_status_code(client: Client):
     resposta = client.get('/')
     assert resposta.status_code == 200
+
+
+def test_title(client: Client):
+    resposta = client.get('/')
+    assert_contains(resposta, '<title>Python Pro</title>')
