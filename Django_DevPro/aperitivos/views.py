d1f16cd2b2c0318d309a9182d83a00a2e29254a1
Leonardo Couto <unknown>
2022-10-27 16:11:09 -0300

    [#314] Implementa a exibição de vídeo de acordo com slug
    [Close #314]

--- Django_DevPro/aperitivos/views.py
+++ Django_DevPro/aperitivos/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    video = {'titulo': 'Vídeo Aperitivo: Motivação', 'vimeo_id': 760589226}
+    videos = {
+        'motivacao': {'titulo': 'Vídeo Aperitivo: Motivação', 'vimeo_id': 760589226},
+        'instalacao': {'titulo': 'Vídeo Aperitivo: Instalacao', 'vimeo_id': 764688138}
+    }
+    video = videos[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
