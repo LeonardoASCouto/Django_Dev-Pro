0744b08c80a4b8961dfde6a50d101c29da42be7c
Leonardo Couto <>
2022-10-28 14:44:55 -0300

    [#318] Implementa índice de vídeos do website
    [Close #318]

--- Django_DevPro/aperitivos/urls.py
+++ Django_DevPro/aperitivos/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from Django_DevPro.aperitivos.views import video
+from Django_DevPro.aperitivos.views import video, indice
 
 app_name = 'aperitivos'
 
 urlpatterns = [
     path('<slug:slug>', video, name='video'),
+    path('', indice, name='indice'),
 ]
