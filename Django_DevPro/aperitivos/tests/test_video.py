a06882f20b80a44ab6b8cecaaec3929721c17021
Leonardo Couto <unknown>
2022-11-21 22:46:12 -0300

    [#350] Adiciona biblioteca de facilitação de testes e os refatora para trabalhar com ela. Além disso, desabilita migrações nos testes de Integração Contínua
    [Close #350]

--- Django_DevPro/aperitivos/tests/test_video.py
+++ Django_DevPro/aperitivos/tests/test_video.py
@@ -1,15 +1,14 @@
 import pytest
 from django.urls import reverse
 
+from model_mommy import mommy
 from Django_DevPro.aperitivos.models import Video
 from Django_DevPro.django_assertions import assert_contains
 
 
 @pytest.fixture
 def video(db):
-    v = Video(slug='motivacao', titulo='Vídeo Aperitivo: Motivação', vimeo_id='760589226')
-    v.save()
-    return v
+    return mommy.make(Video)
 
 
 @pytest.fixture
@@ -36,5 +35,6 @@ def test_video_title(resp, video):
 
 def test_video_content(resp, video):
     assert_contains(resp,
-                    f'<iframe src="https://player.vimeo.com/video/{video.vimeo_id}?h=ddccff4790&amp;badge=0&amp;autopause=0'
-                    '&amp;player_id=0&amp;app_id=58479"')
+                    f'<iframe src="https://player.vimeo.com/video/{video.vimeo_id}?h=ddccff4790&amp;badge=0&amp'
+                    f';autopause=0'
+                    f'&amp;player_id=0&amp;app_id=58479"')
