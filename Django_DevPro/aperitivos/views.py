9be99f07c3155aca10d4be155df769fec73d46a2
Leonardo Couto <>
2022-11-21 22:35:35 -0300

    Realiza pequenos ajustes em testes

--- Django_DevPro/aperitivos/views.py
+++ Django_DevPro/aperitivos/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from Django_DevPro.aperitivos.models import Video
 
 videos = [
-    Video(slug='motivacao', titulo='Vídeo Aperitivo: Motivação', vimeo_id=760589226),
-    Video(slug='instalacao', titulo='Vídeo Aperitivo: Instalação', vimeo_id=764688138)
+    Video(slug='motivacao', titulo='Vídeo Aperitivo: Motivação', vimeo_id='760589226'),
+    Video(slug='instalacao', titulo='Vídeo Aperitivo: Instalação', vimeo_id='764688138')
 ]
 
 videos_dct = {v.slug: v for v in videos}
@@ -15,5 +15,5 @@ def indice(request):
 
 
 def video(request, slug):
-    video = videos_dct[slug]
+    video = get_object_or_404(Video, slug=slug)
     return render(request, 'aperitivos/video.html', context={'video': video})
