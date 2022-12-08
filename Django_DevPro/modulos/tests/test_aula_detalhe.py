b42b270733b5ccb20e1c70042f85b489dc0fe856
Leonardo Couto <>
2022-12-08 13:57:19 -0300

    [#389] Cria página de detalhamento de aula
    [Close #389]

---
+++ Django_DevPro/modulos/tests/test_aula_detalhe.py
@@ -0,0 +1,26 @@
+import pytest
+from Django_DevPro.django_assertions import assert_contains
+from django.urls import reverse
+from model_mommy import mommy
+
+from Django_DevPro.modulos.models import Modulo, Aula
+
+
+@pytest.fixture
+def modulo(db):
+    return mommy.make(Modulo)
+
+
+@pytest.fixture
+def aula(modulo):
+    return mommy.make(Aula, modulo=modulo)
+
+
+@pytest.fixture
+def resposta(client, aula):
+    resposta = client.get(reverse('modulos:aula', kwargs={'slug': aula.slug}))
+    return resposta
+
+
+def test_titulo(resposta, aula):
+    assert_contains(resposta, aula.titulo)
