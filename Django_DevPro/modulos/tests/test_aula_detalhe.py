def0836e2ed7242aed83e968d874043483e8be79
Leonardo Couto <>
2022-12-10 16:57:33 -0300

    [#397] Implementa Breadcrumb na página de detalhamento de Aula
    [Close #397]

--- Django_DevPro/modulos/tests/test_aula_detalhe.py
+++ Django_DevPro/modulos/tests/test_aula_detalhe.py
@@ -28,3 +28,8 @@ def test_titulo(resposta, aula):
 
 def test_vimeo_id(resposta, aula):
     assert_contains(resposta, f'src="https://player.vimeo.com/video/{aula.vimeo_id}')
+
+
+def test_modulo_breadcrumb(resposta, modulo: Modulo):
+    assert_contains(resposta,
+                    f'<li class="breadcrumb-item"><a href="{modulo.get_absolute_url()}">{modulo.titulo}</a></li>')
