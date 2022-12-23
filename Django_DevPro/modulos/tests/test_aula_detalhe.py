16504f7a8176f8c99240089f4bd95f9a0081dd50
Leonardo Couto <>
2022-12-22 23:45:42 -0300

    [#423] Restringe acesso a recursos caso usuário não esteja logado
    [Close #423]

--- Django_DevPro/modulos/tests/test_aula_detalhe.py
+++ Django_DevPro/modulos/tests/test_aula_detalhe.py
@@ -17,19 +17,28 @@ def aula(modulo):
 
 
 @pytest.fixture
-def resposta(client, aula):
-    resposta = client.get(reverse('modulos:aula', kwargs={'slug': aula.slug}))
-    return resposta
+def resp_com_usuario_logado(client_com_usuario_logado, aula):
+    return client_com_usuario_logado.get(reverse('modulos:aula', kwargs={'slug': aula.slug}))
 
 
-def test_titulo(resposta, aula):
-    assert_contains(resposta, aula.titulo)
+def test_titulo(resp_com_usuario_logado, aula):
+    assert_contains(resp_com_usuario_logado, aula.titulo)
 
 
-def test_vimeo_id(resposta, aula):
-    assert_contains(resposta, f'src="https://player.vimeo.com/video/{aula.vimeo_id}')
+def test_vimeo_id(resp_com_usuario_logado, aula):
+    assert_contains(resp_com_usuario_logado, f'src="https://player.vimeo.com/video/{aula.vimeo_id}')
 
 
-def test_modulo_breadcrumb(resposta, modulo: Modulo):
-    assert_contains(resposta,
+def test_modulo_breadcrumb(resp_com_usuario_logado, modulo: Modulo):
+    assert_contains(resp_com_usuario_logado,
                     f'<li class="breadcrumb-item"><a href="{modulo.get_absolute_url()}">{modulo.titulo}</a></li>')
+
+
+@pytest.fixture
+def resp_sem_usuario(client, aula):
+    return client.get(reverse('modulos:aula', kwargs={'slug': aula.slug}))
+
+
+def test_usuario_nao_logado_redirect(resp_sem_usuario):
+    assert resp_sem_usuario.status_code == 302
+    assert resp_sem_usuario.url.startswith(reverse('login'))
